1f60b40b96f1deff7313e9248320d1cdbc49eefa
Ed Simon <esimon@netqnet.com>
2016-07-29 23:06:08 -0700

    Add ShiftsBusiness

---
+++ Bus/ShiftsBusiness.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Text;
+using NQN.DB;
+using NQN.Core;
+
+namespace NQN.Bus
+{
+    public class ShiftsBusiness
+    { 
+        public ObjectList<ShiftsObject> SelectShiftsForMonth(int Yr, int Mo)
+        {
+            ShiftsDM dm = new ShiftsDM();
+             
+            ObjectList<ShiftsObject> CurrentShifts = new ObjectList<ShiftsObject>();
+           CurrentShifts = dm.ShiftsForMonth(Yr, Mo);
+            
+            return CurrentShifts;
+        }
+
+        protected int ShiftSort(ShiftsObject x, ShiftsObject y)
+        {
+            int ret = 0;
+            ret = x.ShiftDate.CompareTo(y.ShiftDate);
+            if (ret == 0)
+                ret = x.Sequence.CompareTo(y.Sequence);
+            return ret;
+        }
+    }
+}
